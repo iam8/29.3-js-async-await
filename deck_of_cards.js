b9d1cd842ea148217e9ed20ea61c9179771d43f6
alexm <alex-m@outlook.com>
2023-03-22 17:17:44 -0700

    Replace old code with async/await version

--- deck_of_cards.js
+++ deck_of_cards.js
@@ -4,43 +4,28 @@
 
 const baseUrl = "https://deckofcardsapi.com/api/deck";
 
-// Part 2-1
-const singleCardPromise = axios.get(`${baseUrl}/new/draw/?count=1`);
+$(async function () {
 
-singleCardPromise
-    .then((data) => {
-        card = data.data.cards[0];
-        console.log(`${card.value} of ${card.suit}`);
+    // Part 2-1
+    const cardData = await axios.get(`${baseUrl}/new/draw/?count=1`);
+    const card = cardData.data.cards[0];
 
-        $("#card-1").text(`${card.value} of ${card.suit}`);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
+    console.log(`${card.value} of ${card.suit}`);
+    $("#card-1").text(`${card.value} of ${card.suit}`);
 
+    // Part 2-2
+    const firstCardData = await axios.get(`${baseUrl}/new/draw/?count=1`);
+    const secondCardData = await axios.get(
+        `${baseUrl}/${firstCardData.data.deck_id}/draw/?count=1`);
 
-// Part 2-2
-const firstCardPromise = axios.get(`${baseUrl}/new/draw/?count=1`);
+    const card1 = firstCardData.data.cards[0];
+    const card2 = secondCardData.data.cards[0];
 
-let card1;
-let card2;
+    console.log(`${card1.value} of ${card1.suit}`);
+    console.log(`${card2.value} of ${card2.suit}`);
 
-firstCardPromise
-    .then((data) => {
-        card1 = data.data.cards[0];
-        return axios.get(`${baseUrl}/${data.data.deck_id}/draw/?count=1`);
-    })
-    .then((data) => {
-        card2 = data.data.cards[0];
-        console.log(`${card1.value} of ${card1.suit}`);
-        console.log(`${card2.value} of ${card2.suit}`);
+    $("#card-2").text(`${card1.value} of ${card1.suit}`);
+    $("#card-3").text(`${card2.value} of ${card2.suit}`);
 
-        $("#card-2").text(`${card1.value} of ${card1.suit}`);
-        $("#card-3").text(`${card2.value} of ${card2.suit}`);
-    })
-    .catch((error) => {
-        console.log(error);
-    });
-
-
-// Part 2-3: See file 'deck_of_cards_full.js'
+    // Part 2-3: See file 'deck_of_cards_full.js'
+});
