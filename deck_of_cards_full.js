9f5d804c89fa205bc9f08d3d611f55bcf17aa406
alexm <alex-m@outlook.com>
2023-03-22 17:27:26 -0700

    Replace old code with async/await version

--- deck_of_cards_full.js
+++ deck_of_cards_full.js
@@ -10,45 +10,34 @@ let deckId;
 const $drawCardBtn = $("#draw-card-btn");
 
 
-function initializeDeck() {
+async function initializeDeck() {
 
     // Create new deck and get a deck ID
-    shuffledDeckPromise = axios.get(`${baseUrl}/new/shuffle`);
+    const newDeckData = await axios.get(`${baseUrl}/new/shuffle`);
+    deckId = newDeckData.data.deck_id;
 
-    shuffledDeckPromise
-        .then((data) => {
-            deckId = data.data.deck_id;
-
-            // Attach event handler to draw-card button
-            $drawCardBtn.on("click", drawCard);
-        })
-        .catch((error) => {
-            console.log(error);
-        });
+    // Attach event handler to draw-card button
+    $drawCardBtn.on("click", drawCard);
 }
 
 
-function drawCard() {
+async function drawCard() {
 
-    const drawCardPromise = axios.get(`${baseUrl}/${deckId}/draw/?count=1`);
-    drawCardPromise
-        .then((data) => {
-            if (data.data.remaining === 0) {
-                $drawCardBtn.hide();
-            }
+    const cardData = await axios.get(`${baseUrl}/${deckId}/draw/?count=1`);
 
-            card = data.data.cards[0];
-            $("#card-display").append(`<image class="card-img" src=${card.image}>`)
+    // Hide draw card button when no cards remain
+    if (cardData.data.remaining === 0) {
+        $drawCardBtn.hide();
+    }
 
-        })
-        .catch((error) => {
-            console.log(error);
-        })
+    // Display card on page
+    card = cardData.data.cards[0];
+    $("#card-display").append(`<image class="card-img" src=${card.image}>`);
 }
 
 
-$(function () {
+$(async function () {
     console.log("DOM is loaded.");
 
-    initializeDeck();
-});
+    await initializeDeck();
+})
